87c620c48b93c4a57e85f4cb1cef52f1c0bad8e9
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-11-23 14:19:33 -0500

    thinking of adding chatgpt support. with limit

--- server/routers/players/routes.py
+++ server/routers/players/routes.py
@@ -6,9 +6,11 @@ playerRoutes = APIRouter()
 
 def_title = Config.db_return_title # default return title, aka index
 
+
 @playerRoutes.post("/api/player/new_player")
 def new_player():
     return { def_title : "New Player created"}
+
 
 @playerRoutes.get("/api/players/player/<player_id>")
 def get_player_by_id(player_id: int):
