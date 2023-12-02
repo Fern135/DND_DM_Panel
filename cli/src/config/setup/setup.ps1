7379e78cfea7105327704fc2c3fe20e1128e72a9
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-12-02 11:08:32 -0500

    set up cli scripts for both mac/linux and windows

---
+++ cli/src/config/setup/setup.ps1
@@ -0,0 +1,8 @@
+# Change directory to the parent directory
+Set-Location ..
+
+# Run the Python setup script
+python3 setup.py
+
+# Install the package
+pip install .
---
+++ cli/src/config/setup/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ../../
+
+python3 setup.py
+pip install . # setting up FR to run the cli setup
