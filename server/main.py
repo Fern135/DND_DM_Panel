de654c2f9c9294fe07412abd4fd6d1731b2b2c4f
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-12-03 11:30:13 -0500

    connected and created sqlite3 db using sqlalchemy

--- server/main.py
+++ server/main.py
@@ -1,9 +1,18 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import os
+
+# config
 from conf.config.config import DEBUG, HOST, API_PORT
 
-from fastapi import FastAPI, Depends
-from fastapi.middleware.cors import CORSMiddleware
+# models
+from .models.player.player import Player  
 
-from routers.players import routes
+# routes
+from .routers.players.routes import playerRoutes
 
 app = FastAPI()
 
@@ -16,30 +25,40 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
+# Get the absolute path to the desired folder
+db_path = './db/'
+
+# Specify the location for the database file in the specified folder
+database_location = f'sqlite:///{os.path.join(db_path, "dnd_dm_panel.db")}'
+
+engine = create_engine(database_location, echo=True)
+Player._session = sessionmaker(bind=engine)
+
+# Create the table in the database
+Player.metadata.create_all(engine)
+
 # Include routers
-app.include_router(routes.playerRoutes)
+app.include_router(playerRoutes)
 
-@app.get("/")
+@app.get("/") # root
 def read_root():
-    return { "SERVER": "Online!!!" }
-
+    return {"SERVER": "Online!!!"}
 
 def runner(debug):
     import uvicorn
     uvicorn.run(
-        "main:app", 
-        host=HOST, 
-        port=API_PORT, 
+        "main:app",
+        host=HOST,
+        port=API_PORT,
         reload=debug
     )
 
-def run(): # running the server
+def run():  # running the server
     match DEBUG:
         case True:
-            runner(True) # auto reload
+            runner(True)  # auto reload
         case _:
             runner(False)
-
 
 # if __name__ == "__main__":
 #     run()
