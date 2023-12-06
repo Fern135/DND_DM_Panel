11f7d7f8e218449e2594a01e656c7963c2722bf7
dante.camblor@gmail.com <dante.camblor@gmail.com>
2023-12-06 16:59:27 -0500

    checked if the data which is dict is empty, which it cannot be

--- server/routers/players/routes.py
+++ server/routers/players/routes.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Body
+from fastapi.responses import JSONResponse
 
 from ...conf.config.config import get_http_codes
 from ...lib.util.util import util
@@ -6,13 +7,23 @@ from ...lib.util.util import util
 playerRoutes = APIRouter()
 
 ut = util()
-new_player_id = ut.generate_random_string(256)
+new_player_id = ut.generate_random_string(256) # will save their "login" as a pin
 
 @playerRoutes.post("/api/player/new_player")
-async def new_player():
-    return { "Server" : f"New Player created with id {new_player_id}"}
+async def new_player(data: dict = Body(...)):
+    return JSONResponse( { "Server" : f"New Player created with id {new_player_id}"} )
 
 
 @playerRoutes.get("/api/players/player/<player_id>")
 async def get_player_by_id(player_id: int):
-    return { "Server" : f"getting player by id, id given {player_id}" }
+    return JSONResponse( { "Server" : f"getting player by id, id given {player_id}" } )
+
+
+@playerRoutes.post("/api/players/player/")
+async def new_char_sheet(data: dict = Body(...)):
+    if data is None:
+        return JSONResponse( { "Server" : "Body cannot be empty" } )
+    
+
+    # parse data into the controller
+    pass
