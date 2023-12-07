267d86970a956c2f317ef1257b6c9d800402d1bd
Fernando Camblor <35276153+Fern135@users.noreply.github.com>
2023-12-07 01:12:38 -0500

    adding get by id and get all, also return self for chaining

--- server/controllers/player/controller.py
+++ server/controllers/player/controller.py
@@ -1,12 +1,12 @@
-
 from ...models.player import Player
 from lib.util.util import util
 
 class PlayerController(util):
 
-    def __init__(self):
-        self.pin                = None
+    def __init__(self) -> None:
         self.temp_pin           = self.generate_random_string(8)
+        self.temp_psw           = self.generate_random_string(16)
+        self.pin                = None
         self.name               = None
         self.race               = None
         self.class_             = None
@@ -22,16 +22,32 @@ class PlayerController(util):
     def setData(self, data:dict) -> None:
         """Set the player's information"""
         if data is not None:
-            self.pin(           data['pin'])
-            self.name(          data['name'])
-            self.race(          data['race'])
-            self.class_(        data['class'])
-            self.level(         data['level'])
-            self.alignment(     data['alignment'])
-            self.background(    data['background'])
-            self.xp_points(     data['xp_points'])
-            self.hit_points(    data['hit_points'])
+            self.pin         = data['pin']
+            self.name        = data['name']
+            self.race        = data['race']
+            self.class_      = data['class']
+            self.level       = data['level']
+            self.alignment   = data['alignment']
+            self.background  = data['background']
+            self.xp_points   = data['xp_points']
+            self.hit_points  = data['hit_points']
 
             # figure out how to handle with
-            self.profile_picture(data['profile_picture'])
-            self.character_picture(data['character_picture'])
+            self.profile_picture    = data['profile_picture']
+            self.character_picture  = data['character_picture']
+
+    
+    def new_player(self) -> bool:
+        """Create a new player and save in database"""
+        
+        return self
+
+    def get(self, id) -> dict:
+        """Get player by ID"""
+
+        return self
+    
+    def get_all(self) -> dict:
+        """Get all players"""
+
+        return self
